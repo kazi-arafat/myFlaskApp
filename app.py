b0ff29d996f45f5a3723c11960811c64b0b173de
Kazi-arafat <arafat.kazi2448@gmail.com>
2019-11-12 01:44:45 +0530

    Register Page Implemented

--- app.py
+++ app.py
@@ -1,12 +1,67 @@
 #Entry file for python backend
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,url_for,session,logging,redirect,request
+from data import GetArticles
+from flask_mysqldb import MySQL
+from wtforms import StringField,TextAreaField,PasswordField,validators,Form
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
+articles = GetArticles()
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Abc@123'
+app.config['MYSQL_DB'] = 'myFlaskApp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor' # By default return ds of my SQL
+
+# initialize Cursor
+mysql = MySQL(app)
 
 @app.route("/")
-def index():
+def Index():
     return render_template("home.html")
-    
+
+@app.route("/about")
+def About():
+    return render_template("about.html")
+
+@app.route("/articles")
+def Articles():
+    return render_template("articles.html",articles = articles)
+
+@app.route("/article/<string:id>")
+def Article(id):
+    return render_template("article.html",id=id)
+
+
+class RegisterForm(Form):
+    name = StringField("Name ",[validators.Length(min=1,max=50)])
+    username = StringField("Username ",[validators.Length(min=4,max=50)])
+    email = StringField("Email",[validators.Length(min=6,max=100)])
+    password = PasswordField("Password",[validators.DataRequired(),
+    validators.EqualTo("confirm",message="Password do not match")])
+    confirm = PasswordField("Confirm Password")
+
+@app.route("/register", methods=["GET","POST"])
+def Register():
+    form = RegisterForm(request.form)
+    if (request.method == 'POST' and form.validate()):
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create Cursor
+        with mysql.connection.cursor() as cur:
+            cur.execute("INSERT INTO user(name,email,username,password) VALUES('{0}','{1}','{2}','{3}')".format(name,email,username,password))
+            # commit to db
+            mysql.connection.commit()
+        flash("Registered Successfully",'success')
+        return redirect(url_for("Index"))
+    return render_template("register.html",form=form)
 
 if (__name__ == '__main__'):
+    app.secret_key = 'secret123'
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,75 @@
+def GetArticles():
+    articles = [
+        {
+        "id" : 1,
+        "title" : "Article One",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Kazi Arafat",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 2,
+        "title" : "Article Two",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Kazi Aftara",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 3,
+        "title" : "Article Three",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Suahana Razi",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 4,
+        "title" : "Article Four",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Kazi Arafat",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 5,
+        "title" : "Article Five",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Suhana Tasneem Razi",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 6,
+        "title" : "Article Six",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Rashmi Bhattacharya",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 7,
+        "title" : "Article Seven",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Samar Kaiyyum",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 8,
+        "title" : "Article Eight",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Kazi Arafat",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 9,
+        "title" : "Article Nine",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Kazi Aftara",
+        "date_created" : "11/11/2019"
+        },
+        {
+        "id" : 10,
+        "title" : "Article Ten",
+        "body": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, ut voluptates repellat aliquam minus voluptatum hic nihil aliquid ipsam accusantium omnis dolore quod consequatur, quisquam blanditiis, beatae molestiae! Illo, dignissimos?",
+        "author" : "Kazi Monami",
+        "date_created" : "11/11/2019"
+        }
+
+        ]
+    return articles
